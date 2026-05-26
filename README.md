d39e8988e575764b8651740ce376ea9abb7ba16f
AAYUSH AWASTHI <aayushawasthi311@gmail.com>
2026-05-26 18:31:02 +0530

    add user and vedio model

--- README.md
+++ README.md
@@ -19,4 +19,39 @@ While connecting a database
 ->cors:CORS stands for Cross-Origin Resource Sharing
 It’s a security feature built into web browsers that controls how web pages can request resources (like APIs, images, or scripts) from a different domain than the one they were loaded from.->middleware
 
-middle-ware => supoose you type the url and then its checked whetehr you are logged in and then data is shown to you , this checking part is called middleware
+->middle-ware => supoose you type the url and then its checked whetehr you are logged in and then data is shown to you , this checking part is called middleware
+
+->libraries:
+->mongoose-aggregattion :
+Perform operations on multiple documents and produce computed results.
+
+
+Both JWT and bcrypt are core technologies used in authentication systems
+
+->bcrypt:helps Hashing passwords securely
+*You CANNOT convert hash back to original password.
+
+->jwt(jasonwebtoken):is a bearer token ,used for Authentication and Authorization
+Simple Meaning
+
+JWT is like:
+
+Digital identity card
+After login, server gives token.
+User sends token in future requests.
+Server verifies token and identifies user.
+
+Why JWT Needed
+Without JWT:
+Server would need to store sessions.
+JWT enables:
+Stateless authentication
+meaning:
+server does not store login session
+
+->HOOkS:
+are middlewares,Hooks allow functional components to use React features.
+
+->in env accesstoken we wont save in database but refresh token we will save
+
+
---
+++ src/models/user.model.js
@@ -0,0 +1,94 @@
+import mongoose ,{Schema} from 'mongoose'
+import jwt from 'jsonwebtoken'
+import bcrypt from "bcrypt"
+
+const userSchema=new Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true //to enable searching but is expensive
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+    },
+    fullname:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true //to enable searching
+    },
+    avatar:{
+        type:String,//cloudinary url 
+        required:true,
+    },
+    coverImage:{
+        type:String
+    },
+    watchHistory:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Vedio"
+        }
+    ],
+    password:{
+        type:String,
+        required:[true,"password is required"]
+    },
+    refreshToken:{
+        type:String
+    }
+
+},{timestamps:true})
+
+
+userSchema.pre('save',async function(next){
+    if(!this.isModified("password")) return next();
+
+    this.password=bcrypt.hash(this.password,10)
+    next()
+})
+
+//this pre is a hook using which we can perfom a task pre of any other funtion
+//do not use arrow function as it does not have this function
+//save  the password only when the pass word is change 
+
+userSchema.methods.isPasswordCorrect=async function(password){
+   return await bcrypt.compare(password,this.password)
+}
+//using methods we can add are own custom  methods
+
+userSchema.methods.generateAccessToken=function(){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname
+    },
+    //Data stored inside token.
+    process.env.ACCESS_TOKEN_SECRET,
+    //Used to sign token securely.
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+    )
+    //sign is used to create token
+}
+userSchema.methods.generateRefreshToken=function(){
+     return jwt.sign({
+        _id:this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+    )
+}
+
+export const User=mongoose.model('User',userSchema)
